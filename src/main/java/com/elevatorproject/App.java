afd6e848338618d61c12aaf5aaf06cbc8dacdea5
Thomas <thomasblc@free.fr>
2019-10-02 21:49:47 +0200

    FAT Jar deployment

--- src/main/java/com/elevatorproject/App.java
+++ src/main/java/com/elevatorproject/App.java
@@ -152,6 +152,6 @@ public class App extends Application{
     }
     
     public static void main(String[] args) {
-        App.launch(args);
+        launch(args);
     }
 }
---
+++ src/main/java/com/elevatorproject/Launcher.java
@@ -0,0 +1,9 @@
+package com.elevatorproject;
+
+
+public class Launcher {
+    public static void main(String[] args) {
+        //App app = new App(10);
+        App.main(args);
+    }
+}
