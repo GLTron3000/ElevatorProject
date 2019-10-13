22fb7092cd80cb3a2d128274832fe5a3dc0a6ee7
Thomas <thomasblc@free.fr>
2019-10-13 20:31:41 +0200

    New smart bugfix 4

--- src/main/java/controlCommand/Smart.java
+++ src/main/java/controlCommand/Smart.java
@@ -14,6 +14,7 @@ public class Smart implements CommandSystem{
     
     @Override
     public void internalButton(int floor) {
+        if(queue.contains(floor)) return;
         System.out.println("[SMART] internal f:"+floor);
         
         if(queue.isEmpty()){
@@ -34,6 +35,7 @@ public class Smart implements CommandSystem{
 
     @Override
     public void externalButton(int floor, boolean downward) {
+        if(queue.contains(floor)) return;
         System.out.println("[SMART] external f:"+floor+" d:"+downward);
         if(isGoingDown()){
             if(!downward) addAtTail(floor, false);
